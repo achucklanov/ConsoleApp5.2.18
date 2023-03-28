067a1e0c4cca3c1db6a01a2d226c33d067c96957
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-28 15:14:52 +0400

    По итогам инд. конс. 28.03.23.

--- ConsoleApp5.2.18/Program.cs
+++ ConsoleApp5.2.18/Program.cs
@@ -2,25 +2,33 @@
 {
     internal class Program
     {
+
+        static int[] arr = new int[10] { 1, 2, -3, -4, 5, -6, 7, 8, 9, 112 };
         static void Main(string[] args)
         {
-            GetArrayFromConsole();
+            var resultUp = SortArray(arr, false);
+            var resultDown = SortArray(arr, true);
+
+            ShowArray(resultDown);
+            ShowArray(resultUp);
+
+            //   GetArrayFromConsole();
         }
 
-        static int[] GetArrayFromConsole()
-        {
-            var result = new int[10];
+        //static int[] GetArrayFromConsole()
+        //{
+        //    var result = new int[10];
 
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-                result[i] = int.Parse(Console.ReadLine());
-            }
+        //    for (int i = 0; i < result.Length; i++)
+        //    {
+        //        Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+        //        result[i] = int.Parse(Console.ReadLine());
+        //    }
 
-            return result;
-        }
+        //    return result;
+        //}
 
-        static int[] SortArray(int[] result)
+        static int[] SortArray(int[] result, bool isSortedUp)
         {
 
             int temp = 0;
@@ -29,32 +37,48 @@
             {
                 for (int j = i + 1; j < result.Length; j++)
                 {
-                    if (result[i] > result[j])
+                    if (isSortedUp)
                     {
-                        temp = result[i];
-                        result[i] = result[j];
-                        result[j] = temp;
+                        if (result[i] > result[j])
+
+                        {
+                            temp = result[i];
+                            result[i] = result[j];
+                            result[j] = temp;
+                        }
+
+                    }
+                    else
+                    {
+                        if (result[i] < result[j])
+                        {
+                            temp = result[i];
+                            result[i] = result[j];
+                            result[j] = temp;
+                        }
                     }
                 }
+
             }
             return result;
+        }
 
-            static void ShowArray(int[] array, bool IsSort = false)
+        static void ShowArray(int[] array, bool IsSort = false)
+        {
+
+            //var result = new int[10];
+
+            //if (IsSort)
+            //{
+            //result = SortArray(arr);
+            //}
+            foreach (var item in arr)
             {
-                var temp = array;
-
-                if (IsSort)
-                {
-                    temp = SortArray(array);
-                }
-                foreach (var item in temp)
-                {
-                    Console.WriteLine(item);
-                }
-
-                var array = GetArrayFromConsole(10);
-                ShowArray(array, true);
+                Console.WriteLine(item);
             }
+
+
+            //ShowArray(result, true);
         }
     }
 }
