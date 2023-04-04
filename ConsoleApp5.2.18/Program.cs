e3655a3f579d33f8510820df20c73e78ff672d1f
AndreyChuck <a.chucklanov@yandex.ru>
2023-04-05 01:08:26 +0400

    Повторение и разбор. Поиск неисправности.

--- ConsoleApp5.2.18/Program.cs
+++ ConsoleApp5.2.18/Program.cs
@@ -3,34 +3,19 @@
     internal class Program
     {
 
-        static int[] arr = new int[10] { 1, 2, -3, -4, 5, -6, 7, 8, 9, 112 };
+        static int[] arr = new int[10] { 1, 2, -3, -4, 5, -6, 7, 8, 9, 112 }; // почему перед методом Main?
+
         static void Main(string[] args)
         {
-            var resultUp = SortArray(arr, false);
+            var resultUp = SortArray(arr, false);  // поменять местами ЛОЖЬ-ИСТИНА ? В связи с алгоритмом сортировки.
             var resultDown = SortArray(arr, true);
 
             ShowArray(resultDown);
             ShowArray(resultUp);
-
-            //   GetArrayFromConsole();
         }
-
-        //static int[] GetArrayFromConsole()
-        //{
-        //    var result = new int[10];
-
-        //    for (int i = 0; i < result.Length; i++)
-        //    {
-        //        Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-        //        result[i] = int.Parse(Console.ReadLine());
-        //    }
-
-        //    return result;
-        //}
 
         static int[] SortArray(int[] result, bool isSortedUp)
         {
-
             int temp = 0;
 
             for (int i = 0; i < result.Length; i++)
@@ -40,13 +25,11 @@
                     if (isSortedUp)
                     {
                         if (result[i] > result[j])
-
                         {
                             temp = result[i];
                             result[i] = result[j];
                             result[j] = temp;
                         }
-
                     }
                     else
                     {
@@ -58,27 +41,16 @@
                         }
                     }
                 }
-
             }
             return result;
         }
 
-        static void ShowArray(int[] array, bool IsSort = false)
+        static void ShowArray(int[] array, bool IsSort = false)  // Почему здесь присваивается false?
         {
-
-            //var result = new int[10];
-
-            //if (IsSort)
-            //{
-            //result = SortArray(arr);
-            //}
             foreach (var item in arr)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item); // Почему выводит дважды один и тот же результат сортировки?
             }
-
-
-            //ShowArray(result, true);
         }
     }
 }
